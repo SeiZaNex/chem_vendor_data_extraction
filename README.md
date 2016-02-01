66b4638f9c3b6de666a377cf32109ce176f42f74
SeiZa <lyoma.guillou@me.com>
2016-02-01 04:29:47 +0100

    final push

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 This is a readme file for the chem_vendor_script
 
+The script works by just running itself.
+It will read EVERY link in the [url_list.txt] file. These links have been handpicked and checked their response beforehand.
+
+The script will generate an output format of the technical info on each web page into the [brc_output.txt]
+
+WARNING: The script is a running loop, you might be waiting for roughly 1-2 hours (depending on the CPU) until the script is done.
+
+
 by Lyoma GUILLOU
--- brc_finechem.py
+++ brc_finechem.py
@@ -30,37 +30,61 @@ def parse_page(page):
     page_undiv = '</div'
     div_count = 0
     div_parse = False
+
+    parse_sec = 0
+
+    text = ""
+    iupac = ""
+    cas = ""
+    mol_mass = ""
+    mol_form = ""
     
     for line in buf:
         if (True == div_parse):
             if (is_target(line, page_div)):
                 div_count += 1
-            print re.sub('\n', '', remove_html_tags(line))
+            if ("" != remove_html_tags(line)):
+                form = remove_html_tags(line)
+                form = re.sub('\r\n', '', form)
+                if (is_target(form, "IUPAC")):
+                    iupac = re.sub("IUPAC: ", '', form)
+                if (is_target(form, "CAS#")):
+                    cas = re.sub("CAS#: ", '', form)
+                if (is_target(form, "Molecular Mass")):
+                    mol_mass = re.sub("Molecular Mass: ", '', form)
+                if (is_target(form, "Molecular Formula")):
+                    mol_form = re.sub("Molecular Formula: ", '', form)
+                if (1 == parse_sec and has_alphanumeric(form)):
+                    text += form
         if (is_target(line, page_undiv)):
             if (div_count > 0):
                 div_count -= 1
             else:
                 div_parse = False
-        if (is_target(line, chem_cat) or is_target(line, chem_des)):
+        if (is_target(line, chem_des)):
             div_parse = True
-            print "tag" 
+            parse_sec += 1
+    array = [iupac, cas, mol_mass, mol_form, text]
+    print array
+    return array
 
 def main():
+    output = open("brc_output.txt", 'w+')
+
     url_list = read_url_list()
-    data = []
 
-    ustream = urllib.urlopen(url_list[0])
-    udata = ustream.read()
-    data.append(udata)
-
-    parse_page(data[0]) #print data[0]
-
-#    ustream = urllib.urlopen("https://www.brc-finechemicals.com/")
-#    udata = ustream.read()
-    
-#    data = remove_html_tags(udata)
-#    print udata
-
+    for url in url_list:
+        print "Opening: " + url
+        ustream = urllib.urlopen(url)
+        udata = ustream.read()
+        array = parse_page(udata)
+        ofstr = url + '\t'
+        for i in array[:-1]:
+            ofstr += i + '\t'
+        ofstr += array[-1] + '\n'
+        output.write(ofstr)
+        print "Page has been formated and written" + '\n'
+    output.close()
 
 if __name__ == '__main__':
     main()
